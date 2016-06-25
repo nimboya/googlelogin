16e90d1126948cbb393adb429b0c8c13059d6f39
Ewere Diagboya <Ewere Diagboya>
2016-06-25 12:36:23 +0100

    Google Login

---
+++ index.php
@@ -0,0 +1,102 @@
+<?php
+session_start(); //session start
+include_once("../../config.php");
+require_once ('libraries/Google/autoload.php');
+
+//Insert your cient ID and secret 
+//You can get it from : https://console.developers.google.com/
+$client_id = ''; 
+$client_secret = '';
+$redirect_uri = 'http://ratemylecturers.com.ng/sociallogin/gauth/index.php';
+
+//incase of logout request, just unset the session var
+if (isset($_GET['logout'])) {
+  unset($_SESSION['access_token']);
+}
+
+/************************************************
+  Make an API request on behalf of a user. In
+  this case we need to have a valid OAuth 2.0
+  token for the user, so we need to send them
+  through a login flow. To do this we need some
+  information from our API console project.
+ ************************************************/
+$client = new Google_Client();
+$client->setClientId($client_id);
+$client->setClientSecret($client_secret);
+$client->setRedirectUri($redirect_uri);
+$client->addScope("email");
+$client->addScope("profile");
+
+/************************************************
+  When we create the service here, we pass the
+  client to it. The client then queries the service
+  for the required scopes, and uses that when
+  generating the authentication URL later.
+ ************************************************/
+$service = new Google_Service_Oauth2($client);
+
+/************************************************
+  If we have a code back from the OAuth 2.0 flow,
+  we need to exchange that with the authenticate()
+  function. We store the resultant access token
+  bundle in the session, and redirect to ourself.
+*/
+  
+if (isset($_GET['code'])) {
+  $client->authenticate($_GET['code']);
+  $_SESSION['access_token'] = $client->getAccessToken();
+  header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+  exit;
+}
+
+/************************************************
+  If we have an access token, we can make
+  requests, else we generate an authentication URL.
+ ************************************************/
+if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+  $client->setAccessToken($_SESSION['access_token']);
+} else {
+  $authUrl = $client->createAuthUrl();
+  header('Location: '. $authUrl);
+}
+
+if (isset($authUrl)){ 
+	$gurl = $authUrl;
+} else {
+	$user = $service->userinfo->get(); //get user info 
+	//$uid = $user->id;
+	//$uname = $user->name;
+	//$uemail = $user->email;
+	$APP_PROFILE['id'] = $user->id;;
+	$APP_PROFILE['name'] = $user->name;
+	$APP_PROFILE['email'] = $user->email;
+	$APP_PROFILE['pswd'] = "google_".$user->id;
+	
+
+	// Input Params
+	$authkey = $config['authkey'];
+	$data = array('authkey'=>$authkey,'fullname'=>$APP_PROFILE['name'],'email'=>$APP_PROFILE['email'],'password'=>$APP_PROFILE['pswd'], 'rpass'=>$APP_PROFILE['pswd']);
+	
+	// Req to WS
+	$ch = curl_init(); 
+	curl_setopt($ch, CURLOPT_URL, $config['sociallogin']); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);	
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	$response = curl_exec($ch);
+	curl_close($ch);
+	
+	$jr = json_decode($response);
+	
+	if($jr->error_code == 0) {
+		$_SESSION['activeuser'] = $APP_PROFILE['email'];
+		header("Location: ../../my");
+	} else {
+		header("Location: .");
+	}
+}
+
+
+?>
+
